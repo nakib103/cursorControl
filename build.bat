1ef1d23cd84bd452034fd2bb988ec0cbc401a8fb
Syed Nakib Hossain <syedhossain103@gmail.com>
2021-05-30 01:00:50 +0600

    cmake - modified build.bat for complete build

--- build.bat
+++ build.bat
@@ -5,12 +5,92 @@ set env=%1
 if %env% == local GOTO :LOCALBUILD
 
 :BUILD
-cmake -B ${{github.workspace}}/build -DCMAKE_BUILD_TYPE=${{env.BUILD_TYPE}}
-cmake --build ${{github.workspace}}/build --config ${{env.BUILD_TYPE}}
 
+:: check if dependent commands are available
+where tar.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo tar.exe not found in path, please install it ..
+    exit /b %ERRORLEVEL%
+)
+where msbuild.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo msbuild.exe not found in path, please install it ..
+    exit /b %ERRORLEVEL%
+)
+where git.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo git.exe not found in path, please install it ..
+    exit /b %ERRORLEVEL%
+)
+where curl.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo curl.exe not found in path, please install it ..
+    exit /b %ERRORLEVEL%
+)
+
+:: create third_party directory
+if not exist "third_party" (
+    mkdir third_party
+)
+
+:: build opencv
+cd third_party
+if not exist "opencv" (
+    @echo "no opencv directory found, installing opencv .."
+    git clone https://github.com/opencv/opencv.git
+    cd opencv
+    git checkout 4.5.1
+
+    if not exist "build" ( 
+        mkdir build
+    )
+    cd build
+
+    cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D INSTALL_CREATE_DISTRIB=ON \
+      -D BUILD_SHARED_LIBS=ON \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D BUILD_LIST=core,imgproc,objdetect,highgui,videoio \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=OFF \
+    ::   -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D WITH_OPENGL=ON  ..
+
+    msbuild OpenCV.sln
+
+    cd ..
+    cd ..
+) else (
+    @echo opencv directory found, assuming opencv installed ..
+)
+
+:: build boost
+curl -L https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz --output boost_1_76_0.tar.gz --silent
+tar -xzf boost_1_76_0.tar.gz
+
+cd ..
+
+:: build cursorcontrol
+
+set BUILD_DIR=build
+if exist %BUILD_DIR% rmdir /s /Q %BUILD_DIR%
+mkdir %BUILD_DIR%
+
+call cmake -B %BUILD_DIR%  -G "Visual Studio 15 2017" -A x64 -T host=x64 ..\..\cursorControl
+call cmake --build %BUILD_DIR%
+
+set INSTALL_DIR=install
+if exist %INSTALL_DIR% rmdir /s /Q %INSTALL_DIR%
+mkdir %INSTALL_DIR%
+call cmake --install %BUILD_DIR% --config Debug --prefix %INSTALL_DIR%
+
+:: build finished
+@echo build finished ..
 GOTO :eof
 
 :LOCALBUILD
+
 set BUILD_DIR=cmake_tests
 if exist %BUILD_DIR% rmdir /s /Q %BUILD_DIR%
 mkdir %BUILD_DIR%
--- build.sh
+++ build.sh
@@ -4,31 +4,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
-# Remove any existing versions of x264
-# sudo apt-get remove x264 libx264-dev
-
-# Install OS Libraries
-
 # Install Dependencies
-sudo apt-get -y install qt5-default tar
-# sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm gfortran git
-# sudo apt-get -y install libjpeg8-dev libjasper-dev libpng12-dev
-# Used for Ubuntu 16.04
-# sudo apt-get -y install libtiff5-dev
-# sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
-# sudo apt-get -y install libxine2-dev libv4l-dev
-# sudo apt-get -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-# sudo apt-get -y install libqt4-dev libgtk2.0-dev libtbb-dev
-# sudo apt-get -y install libatlas-base-dev
-# sudo apt-get -y install libfaac-dev libmp3lame-dev libtheora-dev
-# sudo apt-get -y install libvorbis-dev libxvidcore-dev
-# sudo apt-get -y install libopencore-amrnb-dev libopencore-amrwb-dev
-# sudo apt-get -y install x264 v4l-utils
-
-# Install Optional Dependencies
-# sudo apt-get -y install libprotobuf-dev protobuf-compiler
-# sudo apt-get -y install libgoogle-glog-dev libgflags-dev
-# sudo apt-get -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+sudo apt-get -y install tar
+# sudo apt-get -y install qt5-default
 
 # download OpenCV from Github
 mkdir -p third_party && cd third_party
@@ -50,18 +28,7 @@ mkdir -p build
 cd build
 
 # run CMake with the following options
-# cmake -D CMAKE_BUILD_TYPE=RELEASE \
-#       -D CMAKE_INSTALL_PREFIX=/usr/local \
-#       -D INSTALL_C_EXAMPLES=ON \
-#       -D INSTALL_PYTHON_EXAMPLES=ON \
-#       -D WITH_TBB=ON \
-#       -D WITH_V4L=ON \
-#       -D WITH_QT=ON \
-#       -D WITH_OPENGL=ON \
-#       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
-#       -D BUILD_EXAMPLES=ON ..
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-      # -D WITH_opencv_world=ON \
       -D INSTALL_CREATE_DISTRIB=ON \
       -D BUILD_SHARED_LIBS=ON \
       -D CMAKE_INSTALL_PREFIX=/usr/local \
@@ -69,6 +36,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
       -D WITH_TBB=ON \
       -D WITH_V4L=ON \
       -D WITH_QT=OFF \
+#       -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
       -D WITH_OPENGL=ON  ..
 
 # compile and install

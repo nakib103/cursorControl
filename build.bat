6de4c25a074e25da4a12bf04bea39b3307bcb601
Syed Nakib Hossain <syedhossain103@gmail.com>
2021-05-30 14:34:59 +0600

    build.bat - separeted opencv build for dev option

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
 :: build script for cursorControl package
 
-set env=%~1
+:: create third_party directory
+if not exist "third_party" (
+    mkdir third_party
+)
 
+set env=%~1
 if "%env%" == "dev" (
     GOTO :PACKAGEINSTALL
 ) else (
@@ -30,44 +34,6 @@ where curl.exe >nul 2>nul
 if not ERRORLEVEL 0 (
     @echo curl.exe not found in path, please install it ..
     exit /b %ERRORLEVEL%
-)
-
-GOTO :BUILD
-
-:PACKAGEINSTALL
-:: check if dependent commands are available, if not install them
-where tar.exe >nul 2>nul
-if not ERRORLEVEL 0 (
-    @echo tar.exe not found in path, installing ..
-    choco install tartool
-    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
-)
-where msbuild.exe >nul 2>nul
-if not ERRORLEVEL 0 (
-    @echo msbuild.exe not found in path, installing ..
-    choco install microsoft-build-tools
-    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
-)
-where git.exe >nul 2>nul
-if not ERRORLEVEL 0 (
-    @echo git.exe not found in path, installing ..
-    choco install git.install
-    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
-)
-where curl.exe >nul 2>nul
-if not ERRORLEVEL 0 (
-    @echo curl.exe not found in path, installing ..
-    choco install curl
-    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
-)
-
-GOTO :BUILD
-
-:BUILD
-
-:: create third_party directory
-if not exist "third_party" (
-    mkdir third_party
 )
 
 :: build opencv
@@ -110,8 +76,79 @@ if not exist "boost_1_76_0" (
 )
 cd ..
 
-:: build cursorcontrol
+GOTO :BUILD
 
+:PACKAGEINSTALL
+:: check if dependent commands are available, if not install them
+where tar.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo tar.exe not found in path, installing ..
+    choco install tartool
+    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
+)
+where msbuild.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo msbuild.exe not found in path, installing ..
+    choco install microsoft-build-tools
+    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
+)
+where git.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo git.exe not found in path, installing ..
+    choco install git.install
+    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
+)
+where curl.exe >nul 2>nul
+if not ERRORLEVEL 0 (
+    @echo curl.exe not found in path, installing ..
+    choco install curl
+    if not ERRORLEVEL 0 exit /b %ERRORLEVEL%
+)
+
+:: build opencv
+cd third_party
+if not exist "opencv" (
+    @echo "no opencv directory found, installing opencv .."
+    git clone https://github.com/opencv/opencv.git
+    cd opencv
+    git checkout 4.5.1
+
+    if not exist "build" ( 
+        mkdir build
+    )
+    cd build
+
+    cmake -D CMAKE_BUILD_TYPE=RELEASE `
+      -D INSTALL_CREATE_DISTRIB=ON `
+      -D BUILD_SHARED_LIBS=ON `
+      -D CMAKE_INSTALL_PREFIX=/usr/local `
+      -D BUILD_LIST=core,imgproc,objdetect,highgui,videoio `
+      -D WITH_TBB=ON `
+      -D WITH_V4L=ON `
+      -D WITH_QT=OFF `
+      -D WITH_OPENGL=ON  ..
+
+    msbuild OpenCV.sln
+
+    cd ..
+    cd ..
+) else (
+    @echo opencv directory found, assuming opencv installed ..
+)
+
+:: build boost
+if not exist "boost_1_76_0" (
+    curl -L https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz --output boost_1_76_0.tar.gz --silent
+    tar -xzf boost_1_76_0.tar.gz
+) else (
+    @echo boost directory found, assuming boost installed ..
+)
+cd ..
+
+GOTO :BUILD
+
+:BUILD
+:: build cursorcontrol
 set BUILD_DIR=build
 if exist %BUILD_DIR% rmdir /s /Q %BUILD_DIR%
 mkdir %BUILD_DIR%
